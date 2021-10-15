ed5893c7a1532130b433c331265e32031d64b55e
ryoboy <89571395+ryoboy@users.noreply.github.com>
2021-10-15 15:09:16 +0900

    価格設定以外のバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,18 +1,31 @@
 class Item < ApplicationRecord
 
 
+  with_options presence: true do
+
+    validates :image
+    validates :product_name
+    validates :description
+    validates :price
+  end
+
+    validates :product_type_id, numericality: { other_than: 1 } 
+    validates :product_status_id, numericality: { other_than: 1 } 
+    validates :shipping_charge_id, numericality: { other_than: 1 } 
+    validates :shipping_area_id, numericality: { other_than: 1 } 
+    validates :shipping_day_id, numericality: { other_than: 1 } 
+    validates :price, numericality: true
 
 
+    has_one_attached :image
 
-  
 
-  has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :product_status
-  belongs_to :product_type
-  belongs_to :shipping_area
-  belongs_to :shipping_charge
-  belongs_to :shipping_day
+    belongs_to :product_status
+    belongs_to :product_type
+    belongs_to :shipping_area
+    belongs_to :shipping_charge
+    belongs_to :shipping_day
   
 
 end
