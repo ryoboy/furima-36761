291a6f432d5b68adbdb30add1b0a5eb40fd7a5de
ryoboy <89571395+ryoboy@users.noreply.github.com>
2021-10-15 19:20:52 +0900

    商品出品ページの表示及びテーブルに保存できること確認

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,31 +1,20 @@
 class Item < ApplicationRecord
-
-
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :product_status
+  belongs_to :product_type
+  belongs_to :shipping_area
+  belongs_to :shipping_charge
+  belongs_to :shipping_day
+  has_one_attached :image
   with_options presence: true do
-
-    validates :image
-    validates :product_name
+  validates :image
+   validates :product_name
     validates :description
-    validates :price
-  end
-
     validates :product_type_id, numericality: { other_than: 1 } 
     validates :product_status_id, numericality: { other_than: 1 } 
     validates :shipping_charge_id, numericality: { other_than: 1 } 
     validates :shipping_area_id, numericality: { other_than: 1 } 
     validates :shipping_day_id, numericality: { other_than: 1 } 
-    validates :price, numericality: true
-
-
-    has_one_attached :image
-
-
-  extend ActiveHash::Associations::ActiveRecordExtensions
-    belongs_to :product_status
-    belongs_to :product_type
-    belongs_to :shipping_area
-    belongs_to :shipping_charge
-    belongs_to :shipping_day
-  
-
+    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "¥300~¥9,999,999の間で入力してください"}
+  end
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :item do
-    
+
+    product_name              { Faker::Name.initials(number: 2) }
+    description                 { Faker::Internet.free_email }
+    product_type_id              { '1a' + Faker::Internet.password}
+    product_status_id { password }
+    shipping_charge_id              { Faker::Date.backward }
+    shipping_area_id            { person.last.kanji }
+    shipping_day_id                  { person.first.kanji }
+    price                      { person.last.katakana }
+    name_reading               { person.first.katakana }
+    image
   end
 end
