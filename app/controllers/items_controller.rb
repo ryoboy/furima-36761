aee4e8173ab1cf91f9523a9e8c861b68b1c4aa79
ryoboy <89571395+ryoboy@users.noreply.github.com>
2021-10-20 13:09:24 +0900

    削除機能の実装及びルボコップによる自動修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :contributor_confirmation, only: [:edit, :update]
 
   def index
@@ -25,6 +25,15 @@ class ItemsController < ApplicationController
   end
 
   def edit
+  end
+
+  def destroy
+    if current_user.id == @item.user_id
+      @item.destroy
+      redirect_to root_path
+    else
+      render :show
+    end
   end
 
   def update
