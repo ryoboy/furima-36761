3c3e816e9091ae03fc879e9b02cf2d5df9f86737
ryoboy <89571395+ryoboy@users.noreply.github.com>
2021-10-20 17:25:49 +0900

    購入モデル及び発送先モデルの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,7 @@ class Item < ApplicationRecord
   belongs_to :shipping_day
   has_one_attached :image
   belongs_to :user
+  has_one :purchase
   with_options presence: true do
     validates :image
     validates :product_name
